9e0efc93bd915b6e03b22e41b493ebe6e4888fd6
Masil07 <masilgokul2001@gmail.com>
2024-12-11 17:40:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -29,29 +29,3 @@ def sum_of_primes_in_fibonacci(n):
     fibonacci_series = calculate_fibonacci(n)
     prime_sum = sum(num for num in fibonacci_series if is_prime(num))
     return prime_sum
-
-# Unit tests for the above functions
-if __name__ == "__main__":
-    import unittest
-
-    class TestFunctions(unittest.TestCase):
-        def test_is_prime(self):
-            self.assertTrue(is_prime(2))
-            self.assertTrue(is_prime(3))
-            self.assertFalse(is_prime(4))
-            self.assertFalse(is_prime(1))
-            self.assertTrue(is_prime(17))
-
-        def test_calculate_fibonacci(self):
-            self.assertEqual(calculate_fibonacci(0), [])
-            self.assertEqual(calculate_fibonacci(1), [0])
-            self.assertEqual(calculate_fibonacci(2), [0, 1])
-            self.assertEqual(calculate_fibonacci(5), [0, 1, 1, 2, 3])
-
-        def test_sum_of_primes_in_fibonacci(self):
-            self.assertEqual(sum_of_primes_in_fibonacci(0), 0)
-            self.assertEqual(sum_of_primes_in_fibonacci(1), 0)
-            self.assertEqual(sum_of_primes_in_fibonacci(2), 0)
-            self.assertEqual(sum_of_primes_in_fibonacci(5), 5)  # Only 2 and 3 are primes in [0, 1, 1, 2, 3]
-
-    unittest.main()
